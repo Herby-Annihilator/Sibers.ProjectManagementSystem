d39927bc49ffb707971d36a7c8c98e1654aa7d3c
Rukin Danil <rukin2018d@yandex.ru>
2022-08-28 23:53:01 +0700

    Удаление ненужных директорий

--- Sibers.ProjectManagementSystem.Presentation.Web.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Sibers.ProjectManagementSystem.Presentation.Web.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
